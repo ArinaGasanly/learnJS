208800da1a64332d9cc3baba9a303467cf80a15e
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-12-09 21:40:59 +0300

    добавление урока 2.4

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Урок 2.1
 alert('Я JavaScript!')
+
+
+// Урок 2.4
+let name = 'John';
+let admin;
+
+admin = name;
+
+alert(admin);
+
+
+
+const myPlanetName = 'Земля';
+const currentUserName = 'Вася';
+
